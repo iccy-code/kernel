5e1dad20d2e7a5ff6038a2b8f6720c15ab953664
iccy <iccy.fun@outlook.com>
2020-01-07 23:13:57 +0800

    01-07 初步完成锁

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -4,7 +4,7 @@
 #include "print.h"
 #include "io.h"
 
-#define IDT_DESC_CNT 0x21		// 目前支持的中断数
+#define IDT_DESC_CNT 0x30		// 目前支持的中断数
 
 #define PIC_M_CTRL 0x20			// 主片的控制端口是0x20
 #define PIC_M_DATA 0x21			// 主片的数据端口是0x21
@@ -25,6 +25,7 @@ struct gate_desc {
 static void make_idt_desc(struct gate_desc* p_gdesc, uint8_t attr, intr_handler function);
 
 static struct gate_desc idt[IDT_DESC_CNT];		// idt是中断描述符, 本质就是中断门描述符数组
+
 extern intr_handler intr_entry_table[IDT_DESC_CNT]; 	// 声明引用定义在kernel.asm中的中断处理函数的入口数组
 
 char* intr_name[IDT_DESC_CNT];			// 用于保存异常的名字
@@ -69,6 +70,10 @@ static void pic_init(void) {
 	/* 打开主片上的IR0, 也就是目前只接受时钟产生的中断 */
 	outb(PIC_M_DATA, 0xfe);
 	outb(PIC_S_DATA, 0xff);
+
+	/* 测试键盘, 只打开键盘中断, 其它全部关闭 */
+   outb (PIC_M_DATA, 0xfc);
+   outb (PIC_S_DATA, 0xff);
 
 	put_str("pic_init done\n");
 }
